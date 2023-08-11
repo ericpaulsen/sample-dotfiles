eec8347c89b3c15e7d384aa6222e0be180155695
Eric Paulsen <ericpaulsen@coder.com>
2023-08-10 23:27:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo 'export GITLAB_WORKFLOW_TOKEN="test"' >> ~/.profile
-source ~/.profile
-
 echo "copying VSCode settings"
 cp -f /home/coder/.config/coderv2/dotfiles/settings.json /home/coder/.local/share/code-server/Machine/settings.json
